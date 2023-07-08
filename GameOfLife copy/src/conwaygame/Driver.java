2edc39ec0b96d283017f9c47294c04ccc29bd321
Veena Vrushi <114974799+veenav99@users.noreply.github.com>
2023-07-07 22:38:20 -0400

    Update Driver.java

--- GameOfLife copy/src/conwaygame/Driver.java
+++ GameOfLife copy/src/conwaygame/Driver.java
@@ -1,7 +1,7 @@
 package conwaygame;
-/*
- * Conway's Game of Life Driver
- */
+
+//Conway's Game of Life Driver
+ 
 import java.awt.*;  // For colors
 import java.io.File;
 import java.io.FileWriter;
@@ -9,14 +9,11 @@ import java.io.IOException;
 
 public class Driver {
 
-    // TO STUDENTS: This number is delay in milliseconds between you clicking something and you being able to click
-    //              something else.  This is here because Java is so fast that if you click something, it'll register
-    //              you clicking it multiple times before you let go of the mouse button.  If you want to make this
-    //              responsiveness faster or slower, then decrement or increment this number.  Fidle around with it.
     public static final int DELAY = 250;
 
-    // These are constants, tbh they aren't rlly that useful and are kinda clutter
-    public static final Color BACKGROUND_COLOR = new Color(40, 40, 40);
+    // These are constants
+    
+    public static final Color BACKGROUND_COLOR = new Color( 40 , 40 , 40 );
     public static final int BTN_HALFWIDTH = 10;
     public static final int BTN_HALFHEIGHT = 3;
     public static final int BOARD_HALFWIDTH = 30;
@@ -24,26 +21,34 @@ public class Driver {
     public static final int DEAFULT_ROWS_AND_COLS = 10;
 
     // Arrays for name of each button for each page, include OPTIONS which is on all pages
-    public static final String[] optionNames = {"Back", "Quit"};
-    public static final String[] constructorNames = {"Create Default Game", "Create Game with Input File",
-                                                    "Create Custom Game"};
-    public static final String[] inputNames = {"Open"};
-    public static final String[] createNames = {"-", "+", "-", "+", "Save and Create"};
-    public static final String[] methodNames = {"Cell State", "Is Alive", "Alive Neighbors",
-                                                  "Next Generation", "Next N Generations", "Communities", "Reset", "Save Grid"};
+    
+    public static final String[] optionNames = { "Back" , "Quit" };
+    public static final String[] constructorNames = { "Create Default Game" , "Create Game with Input File" ,
+                                                    "Create Custom Game" };
+    public static final String[] inputNames = { "Open" };
+    public static final String[] createNames = { "-" , "+" , "-" , "+" , "Save and Create" };
+    public static final String[] methodNames = { "Cell State" , "Is Alive" , "Alive Neighbors" ,
+                                                  "Next Generation" , "Next N Generations" , "Communities" , "Reset" , "Save Grid" };
     public static final Button[] OPTIONS = {
-        new Button(6, 95, 4, 3, "Back", true),
-        new Button(16, 95, 4, 3, "Quit", true)
+        new Button( 6 , 95 , 4 , 3 , "Back" , true ) ,
+        new Button( 16 , 95 , 4 , 3 , "Quit" , true )
     };
     
     // Current page user is on, I forget why but there's an important reason I had to make this
+    
     public static Page current;
+    
     // Used for ensuring no weirdness happens with typing filenames
-    public static char[] illegalKeys = {'/', '\\', '|', '"', '*', '<', '>', ':', '?'};
+    
+    public static char[] illegalKeys = { '/' , '\\' , '|' , '"' , '*' , '<' , '>' , ':' , '?' };
+    
     // When switching between pages, these variables keep track of board and buttons on current page
+    
     public static Button[] activeButtons;
     public static Board activeBoard;
+    
     // Used for Next N Generations method
+    
     public static boolean nGen = false;
 
     public static GameOfLife game;
@@ -51,139 +56,150 @@ public class Driver {
 
     // Following is declaring & initializing elements of pages
     // Note there is another initializeElements() method a bit further below
-    // The dimensions of these things are rlly arbitrary btw
+    // The dimensions of these things are rlly arbitrary 
 
     // CONSTRUCTOR elements
-    public static Button[] constructors = new Button[constructorNames.length];
+    public static Button[] constructors = new Button[ constructorNames.length ];
     
     // INPUT elements
-    public static Text inputLabel = new Text(14,50, "Filename:");
-    public static Rectangle inputInput = new Rectangle(42, 50, 20, 3, true);
-    public static Text inputFilename = new Text(24, 50, "", "LEFT");
-    public static Text inputExtension = new Text(64, 50, ".txt");
-    public static Text inputError = new Text(50, 35, "");
-    public static Button inputSubmit = new Button(80, 50, BTN_HALFWIDTH, BTN_HALFHEIGHT, inputNames[0], true);
-    public static Button[] inputs = new Button[]{inputSubmit};
+    
+    public static Text inputLabel = new Text( 14 , 50 , "Filename:" );
+    public static Rectangle inputInput = new Rectangle( 42 , 50 , 20 , 3 , true );
+    public static Text inputFilename = new Text( 24 , 50 , "" , "LEFT") ;
+    public static Text inputExtension = new Text( 64 , 50 , ".txt" );
+    public static Text inputError = new Text( 50 , 35 , "" );
+    public static Button inputSubmit = new Button( 80 , 50 , BTN_HALFWIDTH , BTN_HALFHEIGHT , inputNames[ 0 ] , true );
+    public static Button[] inputs = new Button[]{ inputSubmit };
 
     // CREATE elements
-    public static Button[] createBtns = new Button[createNames.length];
-    public static Board createBoard = new Board(50, 50, 20, 20, DEAFULT_ROWS_AND_COLS, DEAFULT_ROWS_AND_COLS, false, new boolean[DEAFULT_ROWS_AND_COLS][DEAFULT_ROWS_AND_COLS]);
-    public static Text createLabel = new Text(12,8, "Filename:");
-    public static Rectangle createInput = new Rectangle(40, 8, 20, 3, true);
-    public static Text createFilename = new Text(22, 8, "", "LEFT");
-    public static Text createExtension = new Text(62, 8, ".txt");
-    public static Text createError = new Text(50, 16, "");
+    
+    public static Button[] createBtns = new Button[ createNames.length ];
+    public static Board createBoard = new Board( 50 , 50 , 20 , 20 , DEAFULT_ROWS_AND_COLS , DEAFULT_ROWS_AND_COLS , false , new boolean[ DEAFULT_ROWS_AND_COLS ][ DEAFULT_ROWS_AND_COLS ] );
+    public static Text createLabel = new Text( 12 , 8 , "Filename:" );
+    public static Rectangle createInput = new Rectangle( 40 , 8 , 20 , 3 , true );
+    public static Text createFilename = new Text( 22 , 8 , "" , "LEFT" );
+    public static Text createExtension = new Text( 62 , 8 , ".txt" );
+    public static Text createError = new Text( 50 , 16 , "" );
 
     // METHOD elements
-    public static Button[] methods = new Button[methodNames.length];
-    public static Board methodBoard = new Board(65, 45, 30, 30, -1, -1, false, null);
-    public static Text methodText = new Text(65, 90, "Select an Option");
-    public static Rectangle generationsBox = new Rectangle(60, 80, 10, 3, true);
-    public static Text stepsText = new Text(52, 80, "", "LEFT");
-    public static Button methodSubmit = new Button(80, 80, 8, BTN_HALFHEIGHT, "Submit", true);
+    
+    public static Button[] methods = new Button[ methodNames.length ];
+    public static Board methodBoard = new Board( 65 , 45 , 30 , 30 , -1 , -1 , false , null );
+    public static Text methodText = new Text( 65 , 90 , "Select an Option" );
+    public static Rectangle generationsBox = new Rectangle( 60 , 80 , 10 , 3 , true );
+    public static Text stepsText = new Text( 52 , 80 , "" , "LEFT" );
+    public static Button methodSubmit = new Button( 80 , 80 , 8 , BTN_HALFHEIGHT , "Submit" , true );
     public static String methodFilename = "";
 
-    public static void main(String[] args) {
+    public static void main( String[] args ) {
         initializeElements();
         StdDraw.enableDoubleBuffering();
-        StdDraw.clear(BACKGROUND_COLOR);
-        StdDraw.setXscale(0, 100);
-        StdDraw.setYscale(0, 100);
-        StdDraw.setPenColor(StdDraw.WHITE);
+        StdDraw.clear( BACKGROUND_COLOR );
+        StdDraw.setXscale( 0 , 100 );
+        StdDraw.setYscale( 0 , 100 );
+        StdDraw.setPenColor( StdDraw.WHITE );
 
-        displayPage(Page.CONSTRUCTOR);
+        displayPage( Page.CONSTRUCTOR );
         current = Page.CONSTRUCTOR;
 
-        while (true) {
-            if (StdDraw.isMousePressed()) {
+        while ( true ) {
+            if ( StdDraw.isMousePressed() ) {
+                
                 // Check if option buttons in top-left are clicked
-                for (Button btn : OPTIONS) {
-                    if (btn.containsMouse()) {
-                        run(btn);
+                
+                for ( Button btn : OPTIONS ) {
+                    if ( btn.containsMouse() ) {
+                        run( btn );
                     }
                 }
 
                 // Check if buttons on current page are clicked
-                for (Button btn : activeButtons) {
-                    if (btn.containsMouse()) {
-                        run(btn);
+                
+                for ( Button btn : activeButtons ) {
+                    if ( btn.containsMouse() ) {
+                        run( btn );
                     }
                 }
 
                 // Check if filling in grid on CREATE page
-                if (current == Page.CREATE && activeBoard.containsMouse()) {
+                
+                if ( current == Page.CREATE && activeBoard.containsMouse() ) {
                     double mX = StdDraw.mouseX();
                     double mY = StdDraw.mouseY();
 
-                    double[] coords = activeBoard.getCellCM(mX, mY);
-                    int row = (int)Math.round((activeBoard.upperY - coords[0]) / activeBoard.incY);
-                    int col = (int)Math.round((coords[1] - activeBoard.lowerX) / activeBoard.incX);
+                    double[] coords = activeBoard.getCellCM( mX , mY );
+                    int row = ( int ) Math.round( ( activeBoard.upperY - coords[ 0 ] ) / activeBoard.incY );
+                    int col = ( int ) Math.round( ( coords[ 1 ] - activeBoard.lowerX ) / activeBoard.incX );
 
-                    activeBoard.board[row][col] = !(activeBoard.board[row][col]);
-                    displayPage(Page.CREATE);
-                    StdDraw.pause(DELAY);
+                    activeBoard.board[ row ][ col ] = !( activeBoard.board[ row ][ col ] );
+                    displayPage( Page.CREATE );
+                    StdDraw.pause( DELAY );
                 }
             }
 
             // Check if typing on CREATE page
-            if ((current == Page.CREATE || current == Page.INPUT) && StdDraw.hasNextKeyTyped()) {
+            
+            if ( ( current == Page.CREATE || current == Page.INPUT ) && StdDraw.hasNextKeyTyped() ) {
                 char keystroke = StdDraw.nextKeyTyped();
-                String filename = (current == Page.CREATE) ? createFilename.text : inputFilename.text;
+                String filename = ( current == Page.CREATE ) ? createFilename.text : inputFilename.text;
                 int len = filename.length();
-                if ((int)keystroke == 8) { // 8 is backspace in ASCII
-                    if (len > 0) {
-                        filename = filename.substring(0, len - 1);
+                if ( ( int ) keystroke == 8 ) { // 8 is backspace in ASCII
+                    if ( len > 0 ) {
+                        filename = filename.substring( 0 , len - 1 );
                     }
                 } else {
-                    if ((int)keystroke >= 32 && !(inArray(keystroke, illegalKeys)) && len < 20) {
-                        filename = filename.concat(Character.toString(keystroke));
+                    if ( ( int ) keystroke >= 32 && !( inArray(keystroke , illegalKeys ) ) && len < 20 ) {
+                        filename = filename.concat( Character.toString( keystroke ) );
                     }
                 }
-                if (current == Page.CREATE) {
+                if ( current == Page.CREATE ) {
                     createFilename.text = filename;
-                    displayPage(Page.CREATE);
+                    displayPage( Page.CREATE );
                 } else {
                     inputFilename.text = filename;
-                    displayPage(Page.INPUT);
+                    displayPage( Page.INPUT );
                 }
             }
         }
     }
 
-
     public static void initializeElements() {
+        
         // CONSTRUCTOR
-        for (int i = 0; i < constructors.length; i++) {
-            constructors[i] = new Button(50, 60 - 2*(5 + 2*BTN_HALFHEIGHT)*i, 3*BTN_HALFWIDTH, 2*BTN_HALFHEIGHT, constructorNames[i], true);
+        
+        for ( int i = 0; i < constructors.length; i++ ) {
+            constructors[i] = new Button(50, 60 - 2 * ( 5 + 2 * BTN_HALFHEIGHT) * i , 3 * BTN_HALFWIDTH , 2 * BTN_HALFHEIGHT , constructorNames[ i ] , true );
         }
         
         // CREATE
+        
         int pmHW = 3;
-        createBtns[0] = new Button(50, 90, pmHW, pmHW, createNames[0], true);
-        createBtns[1] = new Button(60, 90, pmHW, pmHW, createNames[1], true);
-        createBtns[2] = new Button(50, 80, pmHW, pmHW, createNames[2], true);
-        createBtns[3] = new Button(60, 80, pmHW, pmHW, createNames[3], true);
-        createBtns[4] = new Button(82, 8, 14, BTN_HALFHEIGHT, createNames[4], true);
+        createBtns[ 0 ] = new Button( 50 , 90 , pmHW , pmHW , createNames[ 0 ] , true );
+        createBtns[ 1 ] = new Button( 60,  90 , pmHW , pmHW , createNames[ 1 ] , true );
+        createBtns[ 2 ] = new Button( 50 , 80 , pmHW , pmHW , createNames[ 2 ] , true );
+        createBtns[ 3 ] = new Button( 60 , 80 , pmHW , pmHW , createNames[ 3 ] , true );
+        createBtns[ 4 ] = new Button( 82 , 8 , 14 , BTN_HALFHEIGHT, createNames[ 4 ] , true );
 
         // METHOD
-        for (int i = 0; i < methods.length-2; i++) {
-            methods[i] = new Button(17, 72 - (5 + 2*BTN_HALFHEIGHT)*i, 15, BTN_HALFHEIGHT, methodNames[i], true);
+        
+        for ( int i = 0; i < methods.length - 2; i++ ) {
+            methods[ i ] = new Button( 17 , 72 - ( 5 + 2 * BTN_HALFHEIGHT) * i , 15 , BTN_HALFHEIGHT , methodNames[ i ] , true );
         }
-        methods[methods.length-2] = new Button(90, 10, 8, 2, methodNames[methods.length-2], true);
-        methods[methods.length-1] = new Button(90, 3, 8, 2, methodNames[methods.length-1], true);
+        methods[methods.length - 2 ] = new Button( 90 , 10 , 8 , 2 , methodNames[ methods.length - 2 ] , true );
+        methods[methods.length - 1 ] = new Button( 90 , 3 , 8 , 2 , methodNames[methods.length - 1 ] , true );
     }
 
 
-    public static void displayPage(Page page) {
+    public static void displayPage( Page page ) {
         clearPage();
-        for (Button option : OPTIONS) {
-            option.changeColor(StdDraw.RED);
+        for ( Button option : OPTIONS ) {
+            option.changeColor( StdDraw.RED );
         }
 
         switch(page) {
             case CONSTRUCTOR:
                 Font tmp = StdDraw.getFont();
-                StdDraw.setFont(new Font("SansSerif", Font.PLAIN, 20));
+                StdDraw.setFont( new Font( "SansSerif" , Font.PLAIN , 20 ) );
                 StdDraw.text(50, 90, "Conway's Game of Life");
                 StdDraw.setFont(new Font("SansSerif", Font.PLAIN, 12));
                 StdDraw.text(50, 80, "Click the option to run your constructor with.");
